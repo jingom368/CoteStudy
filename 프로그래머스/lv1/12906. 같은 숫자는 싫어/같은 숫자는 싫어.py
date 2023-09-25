b8cdad5eea05a5f9af92d4ada260b1b76d9621dc
jingom368 <67932739+jingom368@users.noreply.github.com>
2023-09-25 14:49:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 103.92 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,10 +1,23 @@
 def solution(arr):
-    # k = []
-    # for i in range(0,len(arr)-1):
-    #     if arr[i] == arr[i+1]:
-    #         k.append(i)
-    # answer = [arr[i] for i in range(len(arr)) if i not in k]
-    hello = [arr[i] for i in range(len(arr)-1) if arr[i]!=arr[i+1]]
-    hello.append(arr[-1])
+    # hello = [arr[i] for i in range(len(arr)-1) if arr[i]!=arr[i+1]]
+    # hello.append(arr[-1])
+    # answer = hello
+    # 
+    # return answer  
+
+    hello = []
+    # ->
+    # len(arr) : arr의 길이
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]: # 앞에 있는 것이 뒤에 있는 것과 같지 않을 경우에만 넣어주기
+            hello.append(arr[i])
+    hello.append(arr[-1]) # 마지막 원소 넣어주기
     answer = hello
-    return answer
+    return answer  
+    
+    # a = []
+    # answer = a[-1:]
+    # for i in arr:
+    #     if a[-1:] == [i]: continue
+    #     a.append(i)
+    # return a
