c7457aedb58ad5aab8754632ed73e7c0ee6092db
jingom368 <67932739+jingom368@users.noreply.github.com>
2024-03-09 16:42:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,12 +1,25 @@
+// function solution(ineq, eq, n, m) {
+//     if (ineq+eq === "<=") {
+//         answer = Number(n <= m)
+//     } else if (ineq+eq === ">=") {
+//         answer = Number(n >= m)
+//     } else if (ineq+eq === "<!") {
+//         answer = Number(n < m)
+//     } else {
+//         answer = Number(n > m)
+//     }
+//     return answer;
+// }
+
+
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
+
 function solution(ineq, eq, n, m) {
-    if (ineq+eq === "<=") {
-        answer = Number(n <= m)
-    } else if (ineq+eq === ">=") {
-        answer = Number(n >= m)
-    } else if (ineq+eq === "<!") {
-        answer = Number(n < m)
-    } else {
-        answer = Number(n > m)
-    }
-    return answer;
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
 }
